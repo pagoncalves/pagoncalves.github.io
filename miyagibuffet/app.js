70ac5a37444f2f68a1a55b900c65ddc739308d4e
Patricia Goncalves <73037508+pagoncalves@users.noreply.github.com>
2022-06-18 23:44:00 -0300

    Add files via upload

--- miyagibuffet/app.js
+++ miyagibuffet/app.js
@@ -1,1 +1,25 @@
-'use strict';
+//'use strict';
+let time = 3000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#imgtopo img")
+max = images.length
+
+function proximaImagem() {
+
+    images[currentImageIndex].classList.remove("selec")
+
+    currentImageIndex++
+
+    if (currentImageIndex >= max){
+        currentImageIndex = 0
+}
+    images[currentImageIndex].classList.add("selec")
+}
+
+function inicio() {
+    setInterval(() => {
+        // troca de image
+        proximaImagem()
+    }, time)
+}
+window.addEventListener("load", inicio)
